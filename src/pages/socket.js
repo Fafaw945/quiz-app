e7f1519b56c74038fe0d8c894d34fddcc1842909
Fawzi <fawzi.youjil@oclock.school>
2025-11-03 19:01:44 +0100

    correction sessionstorage

--- src/pages/socket.js
+++ src/pages/socket.js
@@ -3,11 +3,11 @@ import { io } from "socket.io-client";
 // 1. Récupérer l'URL du WebSocket avec la syntaxe Create React App
 const WS_URL = process.env.REACT_APP_WS_URL;
 
-// 2. Utiliser cette variable pour la connexion
 export const socket = io(WS_URL, {
   transports: ["websocket"],
   autoConnect: false,
 });
 
 // NOTE : Les logs de connect/disconnect sont gérés dans Lobby.jsx pour plus de contrôle.
+
 
